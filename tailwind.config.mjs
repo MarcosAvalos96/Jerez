1213f4dd0f332904e55f2ae03b0b161a43e90760
Leo0114 <leonel0614@gmail.com>
2024-05-10 18:07:11 -0600

    ft:AlmostDone

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: { primary: "#1e53a1", secondary: "#fefeff", Fondo: "#F7F9FF" },
+      colors: {
+        primary: "#182c6c",
+        Fondo: "#ffffff",
+        FooterF: "#f8f4ec",
+        cardBg: "#f7f7f7",
+        subTitle: "#4779bb",
+        textColor: "#222222",
+      },
       backgroundImage: {
         heroMain:
-          "linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2)), url('/src/assets/images/mundos.webp')",
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2)), url('/src/assets/images/Imagen1.webp')",
       },
     },
   },
