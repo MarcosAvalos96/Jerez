bb098ecb35023c0e3898deaac1c19fcb6a82b1a4
MarcosAvalos96 <mavalos8@ucol.mx>
2024-05-14 00:05:05 -0600

    prueba para subir todoa main

--- src/components/common/PruebaR.jsx
+++ src/components/common/PruebaR.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 function PruebaR() {
-  return <div>PruebaRRRRRRRRRRRRRRRRR</div>;
+  return <div>Prueba para subir a main todo</div>;
 }
 
 export default PruebaR;
