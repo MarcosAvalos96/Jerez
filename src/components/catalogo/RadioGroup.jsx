249a0ec42e44c46ec06b76cd739ac8cda85105db
MarcosAvalos96 <mavalos8@ucol.mx>
2024-05-16 20:03:57 -0600

    texto cambiado

--- src/components/catalogo/RadioGroup.jsx
+++ src/components/catalogo/RadioGroup.jsx
@@ -1,14 +1,10 @@
-
-import {RadioGroup, Radio} from "@nextui-org/react";
+import { RadioGroup, Radio } from "@nextui-org/react";
 
 export default function RGComponet() {
   return (
-    <RadioGroup
-      orientation="horizontal"
-    >
-      <Radio value="buenos-aires">Aplicar solo algunos filtros</Radio>
-      <Radio value="sydney">Aplicar todos los filtros</Radio>
-   
+    <RadioGroup orientation="horizontal">
+      <Radio value="buenos-aires">Que cumpla alguno de los filtros</Radio>
+      <Radio value="sydney">Que cumpla todos los filtros</Radio>
     </RadioGroup>
   );
 }
